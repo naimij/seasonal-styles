c5eeebbd1d818d21f950b25e2ff7f2f721332206
Naimi <naimij@amazon.com>
2023-02-01 20:50:18 -0800

    added prepends

--- js/seasonal-styles.js
+++ js/seasonal-styles.js
@@ -1,4 +1,22 @@
 $("document").ready(function(){
-    //alert("hi from jQuery. This is a shorter version!");
-    $("div#myDiv").css("background-color","green");
+    let counter = 0;
+    $("div#myDiv p").each(function(){
+        counter++;
+        $(this).prepend(counter + "] ");
+    //code goes here 
+    });
+
 });
+
+/*
+let myPTags = document.querySelectorAll("div#myDiv p");
+
+console.log(myPTags);
+*/
+
+/*
+
+document.querySelector("div#myDiv").style.backgroundColor="yellow";
+
+document.querySelector("div#myDiv").innerHTML ="Here is some new text!";
+*/
