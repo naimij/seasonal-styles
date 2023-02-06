5b79797a0a2cdbc11404b9c8998f50cfb10d38bb
Naimi <naimij@amazon.com>
2023-02-05 19:10:21 -0800

    added spring pic

--- js/seasonal-styles.js
+++ js/seasonal-styles.js
@@ -1,22 +1,29 @@
 $("document").ready(function(){
-    let counter = 0;
-    $("div#myDiv p").each(function(){
-        counter++;
-        $(this).prepend(counter + "] ");
-    //code goes here 
+    $('.seasons a').click(function(e){//find all a tags inside class of seasons
+        e.preventDefault();//stop default submission
+        var season = $(this).attr("href");//contents of href attribute of this element
+        //alert(season);
+
+
+        season = season.toLowerCase();
+
+        switch(season){
+
+            case "spring":
+                $("#logo").attr("src","images/spring.gif");
+               
+
+               // #2B7129
+
+               $("html").css("background-color","#2B7129");
+
+            
+            break;
+
+            default:
+                alert("You didn't hit a case!");
+
+
+        }
     });
-
 });
-
-/*
-let myPTags = document.querySelectorAll("div#myDiv p");
-
-console.log(myPTags);
-*/
-
-/*
-
-document.querySelector("div#myDiv").style.backgroundColor="yellow";
-
-document.querySelector("div#myDiv").innerHTML ="Here is some new text!";
-*/
